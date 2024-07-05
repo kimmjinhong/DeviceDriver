addb410dd11b6a1406c0426cf2c619927c096fab
jinhong <jh0703.kim@gmail.com>
2024-07-05 10:07:06 +0900

    read 5 & exception

--- DeviceDriver/DeviceDriver.cpp
+++ DeviceDriver/DeviceDriver.cpp
@@ -1,12 +1,22 @@
 #include "DeviceDriver.h"
+#include <stdexcept>
 
 DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 {}
 
 int DeviceDriver::read(long address)
 {
-    // TODO: implement this method properly
-    return (int)(m_hardware->read(address));
+    // TODO: implement this method properl
+    int result;
+    result = (int)(m_hardware->read(address));
+    result = (int)(m_hardware->read(address));
+    result = (int)(m_hardware->read(address));
+    result = (int)(m_hardware->read(address));
+    result = (int)(m_hardware->read(address));
+    if (address == 0x100) {
+        throw std::runtime_error("Read Fail");
+    }
+    return result;
 }
 
 void DeviceDriver::write(long address, int data)
--- DeviceDriverTest/test.cpp
+++ DeviceDriverTest/test.cpp
@@ -1,9 +1,44 @@
 #pragma once
+#include <string>
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
+#include "../DeviceDriver/DeviceDriver.cpp"
+#include "../DeviceDriver/FlashMemoryDevice.h"
+
+using namespace std;
 using namespace testing;
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+class MockHardware : public FlashMemoryDevice {
+public:
+	MOCK_METHOD(unsigned char, read, (long address), (override));
+	MOCK_METHOD(void, write, (long address, unsigned char data), (override));
+};
+
+class HardwareFixture : public testing::Test {
+protected:
+	void SetUp() override {
+		EXPECT_CALL(mock_hw, read, (), ())
+			.Times(5);
+	}
+public:
+	MockHardware mock_hw;
+};
+
+TEST_F(HardwareFixture, BasicCallTest) {
+
+	DeviceDriver driver(&mock_hw);
+	driver.read(0x00);
+}
+
+TEST_F(HardwareFixture, ReadExceptionTest) {
+	try {
+		DeviceDriver driver(&mock_hw);
+		int result = driver.read(0x100);
+		FAIL();
+	}
+	catch(std::runtime_error& e){
+		//assert
+		std::cout << e.what() << std::endl;
+		EXPECT_EQ(string{ e.what() }, string{ "Read Fail" });
+	}
 }
